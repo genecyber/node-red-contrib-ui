f4309f894edfb86f2d7d478da07a7dc5441a68e3
Shannon Code <genecyber@gmail.com>
2016-03-15 18:02:41 -0400

    trying to make script from text node

--- nodes/ui_script.js
+++ nodes/ui_script.js
@@ -3,8 +3,35 @@ module.exports = function(RED) {
 
     function ScriptNode(config) {
         RED.nodes.createNode(this, config);
-        var node = this;       
-        var done = ui.addScript(config.name, config.link, config.icon, config.order)        
+        var node = this;
+        
+        var tab = RED.nodes.getNode(config.tab);
+        if (!tab) return;
+        
+        var done = ui.add({
+            emitOnlyNewValues: false,
+            node: node, 
+            tab: tab, 
+            group: config.group, 
+            control: {
+                type: 'script',
+                label: config.name,
+                order: config.order,
+                format: config.format
+            },
+            beforeEmit: function(msg, value) {
+                var properties = Object.getOwnPropertyNames(msg).filter(function (p) {return p[0] != '_';});
+                var clonedMsg = { };
+                
+                for (var i=0; i<properties.length; i++) {
+                    var property = properties[i];
+                    clonedMsg[property] = msg[property];
+                }
+                
+                return { msg: clonedMsg };
+            }
+        });
+
         node.on("close", done);
     }
 
