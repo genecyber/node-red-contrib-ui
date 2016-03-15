2c80d2d754127d2b97817ba4da3c4e9565f28076
Shannon Code <genecyber@gmail.com>
2016-03-15 17:52:10 -0400

    another one

--- nodes/ui_script.js
+++ nodes/ui_script.js
@@ -4,35 +4,7 @@ module.exports = function(RED) {
     function ScriptNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;       
-        //var done = ui.addScript(config.name, config.link, config.icon, config.order)    
-        var done = ui.add({
-            forwardInputMessages: true,
-            storeFrontEndInputAsState: true,
-            emitOnlyNewValues: false,
-            node: node, 
-            tab: tab, 
-            group: config.group, 
-            control: {
-                type: 'script',
-                order: config.order,
-                format: config.format
-            },
-            beforeEmit: function(msg, value) {
-                var properties = Object.getOwnPropertyNames(msg).filter(function (p) {return p[0] != '_';});
-                var clonedMsg = { };
-                
-                for (var i=0; i<properties.length; i++) {
-                    var property = properties[i];
-                    clonedMsg[property] = msg[property];
-                }
-                
-                return { msg: clonedMsg };
-            },
-            beforeSend: function (msg, original) {
-                if (original)
-                    return original.msg;
-            }
-        }); 
+        var done = ui.addScript(config.name, config.link, config.icon, config.order)        
         node.on("close", done);
     }
 
